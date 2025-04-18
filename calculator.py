80c3fac43c60a9522145fc738fc756d24ab5a788
Anton Litvin <litvinantoha098@gmail.com>
2025-04-18 13:52:17 +0200

    update

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ def divide(x, y):
     return x / y
 def multiply_list(listm):
     """Функція множення елементів списку."""
-    result = 0
+    result = 1
     for item in listm:
         result *= item
     return result
